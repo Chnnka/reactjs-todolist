e91a8018aeb0453c4bf02ea6583c3e0f587eb6a5
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-06-19 07:24:59 +0530

    20240619-0724

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    setTodos(newTodoList);
+  }
+
   return (
     <>
-      
+      <TodoInput handleAddTodos={handleAddTodos} />
+      <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,18 @@
-export default function TodoInput(){
-    return (
-        <div>
-            TodoInput
-        </div>
-    );
+export default function TodoInput(props) {
+  const { handleAddTodos,todoValue, setTodoValue } = props;
+  return (
+    <header>
+      <input value={todoValue} onChange={(e)=>{
+        setTodoValue(e.target.value)
+      }} type="text" placeholder="Enter Task" />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue('')
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
 }
